75b672d0dd7e26ef895fe6ef564b9bf88f0bbf7e
khaled-nj <khalednjim@gmail.com>
2022-02-16 19:12:01 +0100

    First push

---
+++ PidevJava/src/pidevjava/PidevJava.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pidevjava;
+
+import java.sql.Date;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author macbook
+ */
+public class PidevJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws SQLException {
+        // TODO code application logic here
+       
+    }
+    
+}
